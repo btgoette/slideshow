f05fe66fa0515adf263382e59022d415a863c7bb
Bryan Goette <bryantgoette@hotmail.com>
2023-06-05 16:29:08 -0500

    import react-image-gallery

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,36 @@
-import SlideShow from 'components/SlideShow'
+import ImageGallery from 'react-image-gallery';
 
-let slides = [{
-  img: ""
-}]
+const images = [
+  {
+    original: "http://placekitten.com/1920/1080",
+    thumbnail: "http://placekitten.com/250/150",
+  },
+  {
+    original: "http://placekitten.com/1920/1080",
+    thumbnail: "http://placekitten.com/250/150",
+  },
+  {
+    original: "http://placekitten.com/1920/1080",
+    thumbnail: "http://placekitten.com/250/150",
+  },
+  {
+    original: "http://placekitten.com/1920/1080",
+    thumbnail: "http://placekitten.com/250/150",
+  },
+  {
+    original: "http://placekitten.com/1920/1080",
+    thumbnail: "http://placekitten.com/250/150",
+  },
+  {
+    original: "http://placekitten.com/1920/1080",
+    thumbnail: "http://placekitten.com/250/150",
+  }
+]
 
 export default function Home() {
   return (
     <main>
-      <SlideShow {...slides} />
+      <ImageGallery items={images} />
     </main>
   )
 }
